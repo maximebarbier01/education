e8e383c202ed52d4236766ef4595bda1c522b452
Maxime <maximebarbier57@gmail.com>
2025-06-18 10:44:15 +0200

    Connect PostgreSQL
    Conection entre Postgre SQL et VS Code

--- script.py
+++ script.py
@@ -2,15 +2,14 @@ import psycopg2
 
 conn = psycopg2.connect(
     dbname="education",
-    user="postgres",
+    user="maximebarbier",
     password="@udrey29Le",
     host="localhost",
     port="5432"
 )
 
 cursor = conn.cursor()
-cursor.execute("SELECT version();")
-print(cursor.fetchone())
-
+cursor.execute("SELECT * FROM annuaire LIMIT 1;")
+print(cursor.fetchall())
 cursor.close()
 conn.close()
